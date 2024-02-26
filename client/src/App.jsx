f92c483dfd7f3a941132235f04f468e2b8ea170e
ghost <samuelbrownai@gmail.com>
2024-02-26 20:52:51 +0000

    updated app, added fetch friends, usestate and useeffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [friends, setFriends] = useState([])
+
+  useEffect(() => {
+    fetchFriends()
+  }, [])
+
+  const fetchFriends = async () => {
+    const response = await fetch("http://127.0.0.1:5000/friends")
+    const data = await response.json()
+
+    setFriends(data.friends)
+  }
+
   return (
     <>
       

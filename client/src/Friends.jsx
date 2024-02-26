5447c5912848c5bce0d58c4d754e4f83d317bb99
ghost <samuelbrownai@gmail.com>
2024-02-26 23:08:18 +0000

    updated friends

--- client/src/Friends.jsx
+++ client/src/Friends.jsx
@@ -2,6 +2,24 @@ import React from "react";
 
 
 const Friends = ({friends, updateFriend, updateCallback}) => {
+    const onDelete = async (id) => {
+        try {
+            const options = {
+                method: "DELETE"
+            }
+
+            const response = await fetch(`http://127.0.0.1:5000/delete_friend/${id}`, options)
+
+            if (response.status === 200) {
+                updateCallback();
+            } else {
+                console.error("failed to delete");
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     return <div>
         <h2>Friends</h2>
         <table>
@@ -14,17 +32,17 @@ const Friends = ({friends, updateFriend, updateCallback}) => {
                 </tr>
             </thead>
             <tbody>
-                {friends.map((friend) => {
+                {friends.map((friend) => (
                     <tr key = {friend.id}>
                         <td>{friend.fullName}</td>
                         <td>{friend.email}</td>
                         <td>{friend.workplace}</td>
                         <td>
                             <button onClick={() => updateFriend(friend)}>Update</button>
-                            <button>Delete</button>
+                            <button onClick={() => onDelete(friend.id)}>Delete</button>
                         </td>
                     </tr>
-                })}
+                ))}
             </tbody>
         </table>
     </div>

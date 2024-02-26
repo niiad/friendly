365c835a6171311d3d370621f97069fff1664538
ghost <samuelbrownai@gmail.com>
2024-02-26 22:27:11 +0000

    updated app.jsx, update, and add functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import FriendForm from './FriendFrom';
 function App() {
   const [friends, setFriends] = useState([])
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentFriend, setCurrentFriend] = useState({})
 
   useEffect(() => {
     fetchFriends()
@@ -20,20 +21,37 @@ function App() {
 
   const closeModal = () => {
     setIsModalOpen(false)
+    
+    setCurrentFriend({})
   }  
 
   const openCreateModal = () => {
     if (!isModalOpen) setIsModalOpen(true)
   }
 
+  const openEditModal = (friend) => {
+    if (isModalOpen) {
+      return
+    }
+
+    setCurrentFriend(friend)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+
+    fetchFriends()
+  }
+
   return (
     <>
-      <Friends friends={friends} />
+      <Friends friends={friends} updateFriend={openEditModal} updateCallback={onUpdate}/>
       <button onClick={openCreateModal}>Add</button>
       {isModalOpen && <div className="modal">
         <div className="modal-content">
           <span className="close" onClick={closeModal}>&times;</span>
-          <FriendForm />
+          <FriendForm existingFriend={currentFriend} updateCallback={onUpdate}/>
         </div>
       </div>
         
